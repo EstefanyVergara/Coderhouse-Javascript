b42b835f47a5b9c6c21c36b35310d58e5587867e
EstefanyVergara <vteffa@gmail.com>
2021-10-10 00:07:37 -0300

    Desafio 5/object

---
+++ CLASE 5/app.js
@@ -0,0 +1,31 @@
+class Items {
+  constructor(name, cost, stock) {
+    this.name = name.toLowerCase();
+    this.cost = parseInt(cost);
+    this.stock = stock;
+  }
+
+  sell() {
+    return `Compraste un ${this.name} por el precio de ${this.cost} pesos`;
+  }
+  updateStock() {
+    this.stock = this.stock - 1;
+    return `Ahora quedan ${this.stock} ${this.name}s en bodega`;
+  }
+}
+
+let item1 = new Items("cepillo para gato", 1500, 3);
+let item2 = new Items("platillo para perro", 2500, 7);
+let item3 = new Items("frisbee", 1990, 10);
+
+console.log(item1.sell());
+console.log(item2.updateStock());
+
+//------------INTERACCION CON EL USUARIO
+
+// let nameProduct = prompt(
+// `Hay algún producto que quieres que traigamos para tu mascota, aparte del ${item2.name}?`
+// );
+// let sendProduct = prompt("Cual es el lugar de envio?");
+
+// alert(`Traeremos para ti ${nameProduct} con envio a ${sendProduct}.`);
